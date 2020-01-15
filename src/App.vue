1e19a238b9e10341146c4781e261fada730ef08c
Bona Brian Siagian <bonabriansiagian@engineer.com>
2020-01-15 14:39:50 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Cringe/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cringe from './components/Cringe.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Cringe
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  #app {
+    font-family: 'Lato', sans-serif;
+  }
 </style>
---
+++ src/components/Cringe.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="container my-5 text-center justify-content-center">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Cringe</h2>
+                <h5>{{ description }}</h5>
+                <h5 class="mb-5">by <a href="https://github.com/bonabrian" target="_blank">bonabrian</a></h5>
+            </div>
+        </div>
+        <div class="row justify-content-center mb-4">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <textarea placeholder="wadaw" v-model="cringe" cols="30" rows="5" class="form-control" :class="$style.cringe"></textarea>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header text-center">
+                        <h3 class="card-title">Hasil</h3>
+                        <button data-clipboard-target="#cringe" class="btn btn-outline-success btn-sm font-weight-bold">
+                            Salin
+                        </button>
+                    </div>
+                    <div class="card-body">
+                        <div class="form-group">
+                            <textarea placeholder="widiw" id="cringe" cols="30" rows="5" class="form-control" v-model="this.generated" :class="$style.cringe"></textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style module scoped lang="scss">
+  @import './Cringe.module';
+</style>
+
+<script>
+export default {
+    data() {
+        return {
+            description: 'Untuk para idiotique sejati :)',
+            cringe: ''
+        }
+    },
+    computed: {
+        generated() {
+            let res
+            res = this.cringe.replace(/a|i|u|e|o/gi, 'i')
+
+            return res
+        }
+    }
+}
+</script>
